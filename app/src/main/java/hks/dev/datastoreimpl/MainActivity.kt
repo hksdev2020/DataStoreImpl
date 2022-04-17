bfd5ebd93b29c08f2039bc6abb465dfabe8baa37
S L <>
2022-04-17 13:58:45 +0800

    init project

---
+++ app/src/main/java/hks/dev/datastoreimpl/MainActivity.kt
@@ -0,0 +1,18 @@
+package hks.dev.datastoreimpl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hks.dev.datastoreimpl.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/hks/dev/datastoreimpl/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package hks.dev.datastoreimpl.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
